2d3aacb7b92e188e63b3322142f044c98fa55e92
yejin <yejin@ibleaders.co.kr>
2021-03-30 23:31:05 +0900

    2-5. 웹팩으로 빌드하기
    - webpack 설정은 entry -> module -> output 의 과정
    - 나머지 설정 packge.json 참고

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,32 @@ module.exports = {
   devtool: 'eval',
   // 굳이 파일 확장자를 입력해 주지 않기 위해 사용
   resolve: {
-    extenstions: ['js', 'jsx']
+    extensions: ['.js', '.jsx']
   },
   // 이 두개가 가장 중요
   entry: {
     // 웹팩에서는 한 파일에서 불러온 다른 파일을 알아서 인식하므로 굳이 다 적어주지 않아도 됨. 
-    // import app: ['./client.jsx', './word_relay/wordRelay.jsx']
+    // app: ['./client.jsx', './word_relay/wordRelay.jsx']
     app: ['./client']
-  }, // 입력  
+  }, // 입력
+  
+  // entry에 modules를 적용해서 output으로 넘긴다
+  module: {
+    // 여러개의 규칙을 지정할 수 있음
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+
+      }
+    }]
+
+  },
+  
   output: {
-    path: path.json(__dirname, 'dist'), // __dirname - 현재 위치의 폴더
+    path: path.join(__dirname, 'dist'), // __dirname - 현재 위치의 폴더
     filename: 'app.js'
   } // 출력
 };
--- word_relay/wordRelay.jsx
+++ word_relay/wordRelay.jsx
@@ -3,11 +3,11 @@ const { Component } = require('react');
 
 class WordRelay extends Component {
   state = {
-
+    text: 'Hello, webpack'
   };
 
   render() {
-
+    return <h1>{this.state.text}</h1>;
   }
 }
 
