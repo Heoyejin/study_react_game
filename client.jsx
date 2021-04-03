1b7beffb2876e903d67e04ddca9334a92cb10c05
yejin <yejin@ibleaders.co.kr>
2021-04-03 20:00:07 +0900

    3-1. import와 require 비교
    - 원래 node에서는 import/export를 사용할 수 없지만 babel을 이용하면 import 사용가능
    - export로 모듈을 사용하면 똑같이 import를 사용해줘야함. require로 가져오려 했더니 계속 오류 났음

--- client.jsx
+++ client.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const WordRelay = require('/word_relay/wordRelay_hooks');
+// const WordRelay = require('/word_relay/wordRelay_hooks');
+import NumberBaseball from './number_baseball/numberBaseball'; 
 
-ReactDom.render(<WordRelay />, document.querySelector('#root'));
+ReactDom.render(<NumberBaseball />, document.querySelector('#root'));
--- number_baseball/numberBaseball.jsx
+++ number_baseball/numberBaseball.jsx
@@ -1,16 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class NumberBaseball extends Component {
 
 }
 
-export const hello = 'hello'; // import { hello }
-export const hi = 'hello'; // import { hello, hi }
 
-export default NumberBaseball; // import NumberBaseball; 한번만 사용가능 
-// module.exports랑은 다르지만 호환이 된다.. (?)
 
-// node는 아래 문법만 지원하지만 babel을 사용하면 위의 ex2015문법도 지원 가능하다!
-// const React = require('react');
-// exports.hello = 'hello';
-// module.exports = NumberBaseball;
+export default NumberBaseball;
