57b7feb8f1cf26b83619ca69bb55b76763583a8e
yejin <yejin@ibleaders.co.kr>
2021-04-03 21:30:27 +0900

    3-2. 리액트 반복문(map)
    - javascript의 map 을 이용해서 사용가능
    - 기존 배열 사용방법이랑 동일함

--- number_baseball/numberBaseball.jsx
+++ number_baseball/numberBaseball.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 
 class NumberBaseball extends Component {
+  state = {
+    result: '',
+    value: '',
+    // answer: getNumbers(),
+    tries: []
+  }
 
+  onSubmitForm = (e) => {
+    this.setState({
+      // result: this.state.value'스트라이크' + + '볼',
+
+    });
+  }
+
+  input;
+
+  onChangeInput = (e) => {
+    // input.
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}></input>
+          {/* <button>입력</button> */}
+        </form>
+        <div>남은 횟수: {this.state.tries.length}</div>
+        <ul>
+          {[['a','b'], ['c', 'd'], ['e', 'f'], ['g', 'h'], ['i', 'j']].map((value) => {
+            return <li><b>{value[0]}</b>{value[1]}</li>
+          })}
+        </ul>
+      </>
+    )
+  }
 }
-
-
 
 export default NumberBaseball;
