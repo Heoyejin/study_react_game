d5482ba802be73e65ac82039e3bac1f8282d35a3
yejin <yejin@ibleaders.co.kr>
2021-04-04 17:51:15 +0900

    5-2. setInterval과 라이프 사이클
    - 주로 componentDidMount에서 비동기 처리를 함. (setInterval)
    - componentWillUnmount에서 비동기에 대한 정리를 해줌 (clearInterval)

--- rock_scissors_paper/RockScissorsPaper.jsx
+++ rock_scissors_paper/RockScissorsPaper.jsx
@@ -5,6 +5,17 @@ import React, {Component} from 'react';
  * -> {setState/props} 변경 시 -> shouldComponentUpdate(true) -> render -> componentDidUpdate
  * 부모가 나를 없앤 경우 -> componentWillUnmount
 */
+const rspCoords = {
+  rock: '0',
+  scissor: '-142px',
+  paper: '-284px'
+}
+
+const scores = {
+  rock: 1,
+  scissor: 0,
+  paper: -1
+}
 
 class RockScissorsPaper extends Component {
   state = {
@@ -13,9 +24,28 @@ class RockScissorsPaper extends Component {
     imgCoord: 0
   }
 
-  // 컴포넌트가 첫 랜더링된 후 발생
-  componentDidMount() {
+  interval;
 
+  // 컴포넌트가 첫 랜더링된 후 발생 -> 여기서 주로 비동기 요청을 처리
+  componentDidMount() {
+    
+    this.interval = setInterval(() => {
+      const {imgCoord} = this.state;
+
+      if (imgCoord == rspCoords.rock) {
+        this.setState({
+          imgCoord: rspCoords.scissor
+        });
+      } else if (imgCoord === rspCoords.scissor) {
+        this.setState({
+          imgCoord: rspCoords.paper
+        });
+      } else if (imgCoord === rspCoords.paper) {
+        this.setState({
+          imgCoord: rspCoords.rock
+        });
+      }
+    }, 1000);
   }
 
   // 리 렌더링 후에 실행되는 함수 
@@ -23,9 +53,9 @@ class RockScissorsPaper extends Component {
 
   }
 
-  // 컴포넌트가 제거되기 직전 발생
+  // 컴포넌트가 제거되기 직전 발생 -> 비동기 요청 정리
   componentWillUnmount() {
-
+    clearInterval(this.interval);
   }
 
   onClickBtn = ()=>  {
@@ -33,14 +63,14 @@ class RockScissorsPaper extends Component {
   }
 
   render() {
-    const [result, score, imgCoord] = this.state;
+    const {result, score, imgCoord} = this.state;
     return (
       <>
         <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
         <div>
-          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
-          <button id="scissor" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
-          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
+          <button id="rock" className="btn" onClick={this.onClickBtn('rock')}>바위</button>
+          <button id="scissor" className="btn" onClick={this.onClickBtn('scissor')}>가위</button>
+          <button id="paper" className="btn" onClick={this.onClickBtn('paper')}>보</button>
         </div>
         <div>{result}</div>
         <div>현재 {score}점</div>
