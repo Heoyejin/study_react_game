469dcb8126d55f6bb084847d0ecb30d2626e462b
yejin <yejin@ibleaders.co.kr>
2021-04-07 23:20:29 +0900

    6-3. componentDidUpdate
    - componentDidUpdate는 render가 실행될 때 마다 발생함
    - 여기서 변경된 사항에 대한 처리를 할 수 있음

--- lotto/lotto.jsx
+++ lotto/lotto.jsx
@@ -4,11 +4,14 @@ import Ball from './Ball';
 function getWinNumbers() {
   const candidate = Array(45).fill().map((v, i) => i + 1);
   const shuffle = [];
-  while (candidate.length > 0) { 
+  while (candidate.length > 0) {
+    // splice는 어떤 배열에서 특정 구간을 빼오는 것 !
     shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
   }
   const bonusNumber = shuffle[shuffle.length - 1];
+  // sort 함수를 이용해서 오름차순 정렬
   const winNumbers = shuffle.splice(0, 6).sort((p, c) => p - c);
+  console.log("getWinNumbers", winNumbers, bonusNumber);
   return [...winNumbers, bonusNumber];
 }
 
@@ -45,11 +48,29 @@ class Lotto extends Component {
     this.runtimeouts();
   }
   
+  // 어떤 것이 바뀌었는지 판단 가능
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.winBalls.length == 0) {
+      this.runtimeouts();
+    }
+  }
+  
   componentWillUnmount() {
     this.timeouts.forEach((v) => {
       clearTimeout(v);
     });
   }
+
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getWinNumbers(),  // 당첨 숫자들
+      winBalls: [],
+      bonus: null, // 보너스 공
+      redo: false
+    });
+    this.timeouts = [];
+  }
+
   render() {
     const {winBalls, bonus, redo} = this.state;
     return (
