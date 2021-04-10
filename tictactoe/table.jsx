e284b4ad1f1eefd3dfeac20afa9664ca9684c269
yejin <yejin@ibleaders.co.kr>
2021-04-10 19:44:48 +0900

    7-2. reducer, action, dispatch의 관계
    - useReducer 로 선언한 state를 reducer, dispatch 등을 사용해서 변경

--- tictactoe/table.jsx
+++ tictactoe/table.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tr from './tr';
 
-const Table = () => {
+const Table = ({ onClick, tableData }) => {
   return (
-    <Tr>{}</Tr>
+    <table onclick={onClick}>
+      {Array(tableData.length).fill().map((tr, i) => <Tr rowData={tableData[i]}>{''}</Tr>)}
+    </table>
   )
 }
 export default Table;
--- tictactoe/ticTacToe.jsx
+++ tictactoe/ticTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 import Table from './table';
 
 const initialState = {
@@ -7,8 +7,17 @@ const initialState = {
   tableData: [['','',''], ['','',''], ['','','']]
 };
 
-const reducer = {
+const SET_WINNER = 'SET_WINNER';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_WINNER:
+      // state.winner = action.winner 이렇게 직접 넣어주면 안됨...
+      return {
+        ...state,
+        winner: action.winner
+      };
+  }
 };
 
 const TicTacToe = () => {
@@ -17,11 +26,14 @@ const TicTacToe = () => {
   // const [turn, setTurn] = useState('0');
   // const [tableData, setTableData] = useState([['','',''], ['','',''], ['','','']]);
 
+  const onClickTable = useCallback(() => {
+    dispatch({ type: SET_WINNER, winner: '0'});
+  }, []);
   // 실제 클릭하는건 td 컴포넌트를 클릭하는데 처리는 여기서 해줌 (연결하는 작업이 필요함) 
   return (
     <>
-      <Table></Table>
-      { winner && <div>{winner}님의 승리 !</div>}
+      <Table onClick={onClickTable} tableData={state.tableData}></Table>
+      { state.winner && <div>{state.winner}님의 승리 !</div>}
     </>
   )
 }
