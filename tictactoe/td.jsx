e89878b291611221bb0c27e1b089c247cc39845b
yejin <yejin@ibleaders.co.kr>
2021-04-11 16:11:28 +0900

    7-5. 테이블 최적화 하기
    - 성능 최적화 시 변경 되는 부분을 useRef, useEffect를 사용해서 파악하고 useMemo, memo로 선언 해줄 부분을 찾아서 구분해줘야함
    - memo로 최적화 되지 않으면 useMemo를 사용하기

--- tictactoe/td.jsx
+++ tictactoe/td.jsx
@@ -1,7 +1,17 @@
-import React, { useCallback } from 'react';
+import React, { memo, useCallback, useEffect, useReducer, useRef } from 'react';
 import { CLICK_CELL, CHANGE_TURN } from './ticTacToe';
 
-const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
+const Td = memo(({ rowIndex, cellIndex, cellData, dispatch }) => {
+  console.log("Td rendering");
+  const ref = useRef([]);
+  useEffect(() => {
+    // 성능 최적화 할 때 어느 부분 떄문에 랜더링이 계속 일어나는 지 확인 해봐야함
+    console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], cellData === ref.current[2], dispatch === ref.current[3]);
+    console.log(cellData, ref.current[2]);
+    ref.current = [rowIndex, cellIndex, cellData, dispatch];
+  }, [rowIndex, cellIndex, cellData, dispatch]);
+  
+  // props 로 넣어주는 함수는 useCallback으로 선언해 줘야하고 함수 내부에 바뀌는 데이터를 인자로 넣어줘야함.
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
     // 한번 누른 셀은 다시 못 누르게 변경 
@@ -16,5 +26,5 @@ const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
   return (
     <td onClick={onClickTd}>{cellData}</td>
   )
-}
+})
 export default Td;
--- tictactoe/tr.jsx
+++ tictactoe/tr.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { memo, useEffect, useMemo, useRef}  from 'react';
 import Td from './td';
 
-const Tr = ({ rowData, rowIndex, dispatch }) => {
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
+  console.log("Tr rendering");
+  const ref = useRef([]);
+  useEffect(() => {
+    // 성능 최적화 할 때 어느 부분 떄문에 랜더링이 계속 일어나는 지 확인 해봐야함
+    console.log(rowData === ref.current[0], rowIndex === ref.current[1], dispatch === ref.current[2]);
+    ref.current = [rowData, rowIndex, dispatch];
+  }, [rowData, rowIndex, dispatch]);
   return (
     <tr>
-      {Array(rowData.length).fill().map((td, i) => <Td rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch}>{''}</Td>)}
+      {Array(rowData.length).fill().map((td, i) => 
+      //최후에 수단으로 memo로 최적화가 되지 않으면 useMemo를 사용해야함 !! 최후의 수단 같은 느낌 
+      // useMemo(() => {
+      <Td key={i} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch}>{''}</Td>
+      // }, [rowData[i]])
+      )}
     </tr>
   )
-}
+})
 export default Tr;
