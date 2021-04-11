4e27815d734407a3dfd5435080123bf4b1dcd6ee
yejin <yejin@ibleaders.co.kr>
2021-04-11 16:43:31 +0900

    8-2. createContext와 Provider
    - dispatch를 넘겨줄 최상위에서 createContext로 선언하고 넘겨줄 자식 컴포넌트를 Provider로 감싸서 선언
    - value에 넘겨줄 데이터와 dispatch를 선언해야줘야는데 이 때 Context API는 성능 최적하게 어려운 부분이 많기 때문에 미리 useMemo를 사용해서 선언하기

--- mineSearch/form.jsx
+++ mineSearch/form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
+import TableContext, { START_GAME } from './mineSearch';
 
 const Form = ({ }) => {
   const [row, setRow] = useState(10);
   const [cell, setCell] = useState(10);
   const [mine, setMine] = useState(10);
+  const value = useContext(TableContext);
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value);
@@ -18,6 +20,7 @@ const Form = ({ }) => {
   }, []);
 
   const onClickButton = useCallback((e) => {
+    dispatch({ type: START_GAME, row, cell, mine}, []);
   }, []);
 
   return (
--- mineSearch/mineSearch.jsx
+++ mineSearch/mineSearch.jsx
@@ -1,25 +1,72 @@
-import React, { useReducer } from 'react';
+import React, { createContext, useMemo, useReducer } from 'react';
 import Table from './table';
 import Form from './form';
 
+export const CODE = {
+  MINE: -7,
+  NORMAL : -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0
+};
+
+export const TableContext = createContext({
+  tableData: [
+    [],
+    [],
+    [],
+    [],
+    []
+  ],
+  dispatch: () => {}
+});
+
 // state 선언 구간 
 const initialState = {
+  tableData: [],
+  timer: 0,
+  result: ''
 };
+
+export const START_GAME = 'START_GAME';
 
 // setState 하는 구간 
 const reducer = (state, action) => {
+  switch(action.type) {
+    case START_GAME: 
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine)
+      }
+    default:
+      return state;
+  }
 };
+
+const plantMine = () => {
+
+}
 
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   
+  const value = useMemo(() => ({
+    tableData: state.tableData, 
+    dispatch 
+  }), [state.tableData]);
+
   return (
-    <>
+    // provider로 감싸 줘야 이 내부의 컴포넌트에서 TableContext 사용가능
+    // Context API는 아래 value를 계속 다시 생성하기 때문에  성능 최적화에 어려움이 있음
+    <TableContext.Provider value={value}>
       <Form></Form>
       <div>{state.timer}</div>
       <Table></Table>
       <div>{state.result}</div>
-    </>
+    </TableContext.Provider>
   )
 }
 export default MineSearch;
