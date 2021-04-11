fa7f00653e1b6befa3cbde45bff6f11aaacf5126
yejin <yejin@ibleaders.co.kr>
2021-04-11 15:19:25 +0900

    7-3. action 만들어서 dispatch 하기
    - useReducer는 state - initialState, dispatch-reducer가 동일하게 맵핑됨
    - dispath를 자식 컴포넌트에서 사용하려면 자식 컴포넌트로 전달 해줘야 사용가능
    - 변수를 다른 컴포넌트에서 사용하려면 export 해서 사용하기

--- tictactoe/ticTacToe.jsx
+++ tictactoe/ticTacToe.jsx
@@ -1,13 +1,19 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useReducer, useCallback } from 'react';
 import Table from './table';
 
 const initialState = {
   winner: '',
-  turn: '0',
-  tableData: [['','',''], ['','',''], ['','','']]
+  turn: 'O',
+  tableData: [
+    ['','',''], 
+    ['','',''], 
+    ['','','']
+  ]
 };
 
-const SET_WINNER = 'SET_WINNER';
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,23 +23,41 @@ const reducer = (state, action) => {
         ...state,
         winner: action.winner
       };
+    case CLICK_CELL: {
+      const tableData = [...state.tableData];
+      // ... 는 얕은 복사!
+      tableData[action.row] = [...tableData[action.row]];
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData
+      }
+    }
+    case CHANGE_TURN: {
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O',
+      }
+    }
   }
 };
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, turn, winner, recentCell } = state;
   // const [winner, setWinner] = useState('');
   // const [turn, setTurn] = useState('0');
   // const [tableData, setTableData] = useState([['','',''], ['','',''], ['','','']]);
 
   const onClickTable = useCallback(() => {
-    dispatch({ type: SET_WINNER, winner: '0'});
+    dispatch({ type: SET_WINNER, winner: 'O'});
   }, []);
   // 실제 클릭하는건 td 컴포넌트를 클릭하는데 처리는 여기서 해줌 (연결하는 작업이 필요함) 
   return (
     <>
-      <Table onClick={onClickTable} tableData={state.tableData}></Table>
-      { state.winner && <div>{state.winner}님의 승리 !</div>}
+      {/* dispath를 td 컴포넌트에서 사용하려면 이렇게 전달 해줘야함*/}
+      <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch}></Table>
+      { winner && <div>{winner}님의 승리 !</div>}
     </>
   )
 }
