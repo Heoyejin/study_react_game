304743e7f537e37ba6537a894822493b5995716a
yejin <yejin@ibleaders.co.kr>
2021-04-11 15:50:14 +0900

    7-4. 틱택토 구현하기
    - useReducer는 state 여러개를 한번에 처리 하는 것으로 (Redux의 연장선 느낌)
    - state 선언을 객체로 한번에 함
    - setState는 reducer 함수로 한번에 처리하고 dispatch.type은 Action 의 이름 나머지 인자는 action.인자 로 사용

--- tictactoe/td.jsx
+++ tictactoe/td.jsx
@@ -4,9 +4,14 @@ import { CLICK_CELL, CHANGE_TURN } from './ticTacToe';
 const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
+    // 한번 누른 셀은 다시 못 누르게 변경 
+    if (cellData) {
+      return;
+    }
+    // dispatch에서 state 변경 하는 것은 비동기 !! 
+    // 비동기 state를 처리하려면 무조건 useEffect 사용
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex});
-    dispatch({ type: CHANGE_TURN });
-  }, []);
+  }, [cellData]);
 
   return (
     <td onClick={onClickTd}>{cellData}</td>
--- tictactoe/ticTacToe.jsx
+++ tictactoe/ticTacToe.jsx
@@ -1,6 +1,7 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useEffect, useReducer, useCallback } from 'react';
 import Table from './table';
 
+// state 선언 구간 
 const initialState = {
   winner: '',
   turn: 'O',
@@ -8,13 +9,16 @@ const initialState = {
     ['','',''], 
     ['','',''], 
     ['','','']
-  ]
+  ],
+  recentCell: [-1, -1]
 };
 
 export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL';
 export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESUT_GAME = 'RESUT_GAME';
 
+// setState 하는 구간 
 const reducer = (state, action) => {
   switch (action.type) {
     case SET_WINNER:
@@ -30,7 +34,8 @@ const reducer = (state, action) => {
       tableData[action.row][action.cell] = state.turn;
       return {
         ...state,
-        tableData
+        tableData,
+        recentCell: [action.row, action.cell]
       }
     }
     case CHANGE_TURN: {
@@ -39,10 +44,23 @@ const reducer = (state, action) => {
         turn: state.turn === 'O' ? 'X' : 'O',
       }
     }
+    case RESUT_GAME: {
+      return {
+        ...state, 
+        turn: 'O',
+        tableData: [
+          ['','',''], 
+          ['','',''], 
+          ['','','']
+        ],
+        recentCell: [-1, -1]
+      }
+    }
   }
 };
 
 const TicTacToe = () => {
+  // state 여러개를 한번에 선언하고 처리할 수 있는 useReducer !!
   const [state, dispatch] = useReducer(reducer, initialState);
   const { tableData, turn, winner, recentCell } = state;
   // const [winner, setWinner] = useState('');
@@ -53,6 +71,40 @@ const TicTacToe = () => {
     dispatch({ type: SET_WINNER, winner: 'O'});
   }, []);
   // 실제 클릭하는건 td 컴포넌트를 클릭하는데 처리는 여기서 해줌 (연결하는 작업이 필요함) 
+
+  // 틱택토의 알고리즘은 클릭한 셀에서 체킹해야할 셀들만 검새해주면 됨
+  useEffect (() => {
+    const [row, cell] = recentCell;
+    let win = false;
+    // console.log(row, cell, win, tableData);
+    if (row < 0) return;
+    if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn) {
+      win = true;
+    } else if (tableData[0][cell] === turn && tableData[1][cell] === turn && tableData[2][cell] === turn) {
+      win = true;
+    } else if (tableData[0][cell] === turn && tableData[1][cell + 1] === turn && tableData[2][cell + 2] === turn) {
+      win = true;
+    } else if (tableData[0][cell] === turn && tableData[1][cell - 1] === turn && tableData[2][cell - 2] === turn) {
+      win = true;
+    }
+
+    if (win) {
+      // dispatch가 setState와 같은 Action이고 type이 Action의 이름
+      dispatch({type: SET_WINNER, winner: turn});
+      dispatch({type: RESUT_GAME});
+    } else {
+      let all = true; // all이 true면 게임 끝! 무승부라는 뜻
+      tableData.forEach((row) => {
+        row.forEach((cell) => {
+          // 비어있는 셀이 있으면 계속 게임 진행
+          if (!cell) all = false;
+        })
+      });
+      if (all) dispatch({type: RESUT_GAME});
+      else dispatch({ type: CHANGE_TURN });
+    }
+  }, [recentCell]);
+
   return (
     <>
       {/* dispath를 td 컴포넌트에서 사용하려면 이렇게 전달 해줘야함*/}
