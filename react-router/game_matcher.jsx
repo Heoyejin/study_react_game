b4c23a2925394b0927bc197b855ab501b7916dbc
yejin <yejin@ibleaders.co.kr>
2021-04-14 22:40:05 +0900

    9-4. location, match, history
    - history는 페이지 이동에 대한 내역을 담고 있고 페이지 이동에 대한 API를 가지고 있음 (실제 페이지가 바뀌는게 아니라 실제로는 하나의 페이지이기 때문)
    - match는 실제 주소와 param (:)으로 바꾼 주소를 가지고 있음, 동적 주소 라우팅
    - location는 실제 주소를 가지고 있음
    - history.pushState API 활용시 주소 변경 가능

--- react-router/game_matcher.jsx
+++ react-router/game_matcher.jsx
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
+import RSP from '../rock_scissors_paper/RockScissorsPaper_hooks';
+import Lotto from '../lotto/lotto';
 
 class GameMatcher extends Component {
   render() {
-    console.log(this.props);
+    console.log(this.props);if (this.props.match.params.name === "rock_scissors_paper") {
+      return <Lotto></Lotto>;
+    } else if (this.props.match.params.name === "lotto") {
+      return <RSP></RSP>
+    } 
     return (
-      <div>게임매처</div>
+      <div>
+        일치하는 게임이 없습니다.
+      </div>
     )
   }
 }
+
+// history.pushState는 브라우저의 API이다.
+// React에도 this.props에 history API가 있는데 두 API는 모두 실제 url을 바꿔주는 거임. 근데 React에서는 this.props의 API를 사용하기
 
 // Router 같은 컴포넌트를 사용하지 않는 경우 this.props에 Link로 설정된 컴포넌트 경로들이 없는 경우 withRouter를 사용함
 // export default withRouter(GameMatcher);
--- react-router/games.jsx
+++ react-router/games.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { HashRouter, Route, Link} from 'react-router-dom';
-import NumberBaseball from '../number_baseball/numberBaseball';
-import RSP from '../rock_scissors_paper/RockScissorsPaper_hooks';
-import Lotto from '../lotto/lotto';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
 import GameMatcher from './game_matcher';
 
 const Games = () => {
 
   return (
-    <HashRouter>
+    <BrowserRouter>
     <div>
       {/* <a href=''>숫자야구</a> */}
       <Link to='/game/lotto'>로또</Link> &nbsp;
-      <Link to='/game/rock_scissors_paper'>가위바위보</Link>
+      <Link to='/game/rock_scissors_paper'>가위바위보</Link> &nbsp;
       <Link to='/game/index'>게임 매쳐</Link>
     </div>
     <div>
       <Route path='/game/:name' component={GameMatcher}></Route>
     </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
