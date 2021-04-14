097b8fba2d9375dcf2266bd9577986293ac3a40a
yejin <yejin@ibleaders.co.kr>
2021-04-14 23:07:28 +0900

    9-6. render props, switch, exact
    - 함수 컴포넌트에서 class 컴포넌트로 파라미터 넘기기
    - switch는 여러개의 동일한 주소가 선언되어 있으면 가장 처음 주소를 호출
    - exact는 상위주소와 하위 주소가 동시에 선언되었을 때 하위 주소 호출 시 상위주소도 일치 하다고 생각하여 두 페이지를 모두 띄워줌 이럴 때 사용

--- react-router/games.jsx
+++ react-router/games.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import GameMatcher from './game_matcher';
 
 const Games = () => {
@@ -13,7 +13,20 @@ const Games = () => {
       <Link to='/game/index'>게임 매쳐</Link>
     </div>
     <div>
-      <Route path='/game/:name' component={GameMatcher}></Route>
+      {/* <Route path='/game/:name' component={GameMatcher}></Route> */}
+      {/* GameMatcher로 props 넘기기  */}
+      {/* <Route path='/game/:name' component={() => <GameMatcher props="123456"/>}></Route> */}
+      {/* Switch는 내부에 있는 컴포넌트 중 공통된 게 있으면 일치하는 첫번째 컴포넌트만 출력함 */}
+      <Switch>
+      <Route path='/game/:name' render={(props) => <GameMatcher {...props}/>}></Route>
+      {/* 아래 주소처럼 /game/mineSearch 이런식의 주소는 / 주소도 일치한다고 생각하기 때문에 exact를 사용*/}
+      <Route exact path='/' render={(props) => <GameMatcher {...props}/>}></Route>
+      <Route path='/game/mineSearch' render={(props) => <GameMatcher {...props}/>}></Route>
+      <Route path='/game/mineSearch' render={(props) => <GameMatcher {...props}/>}></Route>
+      <Route path='/game/mineSearch' render={(props) => <GameMatcher {...props}/>}></Route>
+      <Route path='/game/mineSearch' render={(props) => <GameMatcher {...props}/>}></Route>
+      <Route path='/game/mineSearch' render={(props) => <GameMatcher {...props}/>}></Route>=
+      </Switch>
     </div>
     </BrowserRouter>
   );
